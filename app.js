085e06d3b4f88dd533e9bd8b922db8adc3e7eb9e
skarc12 <skarc12@freeuni.edu.ge>
2020-05-04 21:51:39 +0400

    port thing

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ app.get('/', (req, res) => {
     res.send("Home Page.");
 });
 
-app.listen(3000, () => {
+
+const port = process.env.port || 3000;
+app.listen(port, () => {
     console.log('Hey there (::)');
 });
 
